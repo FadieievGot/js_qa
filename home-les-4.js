bd23b5ab1253d7545d2cae496cb234ed600d5834
Oleksii Fadieiev <kreativandd@gmail.com>
2021-05-31 21:37:35 +0300

    home

---
+++ home-les-4.js
@@ -0,0 +1,18 @@
+// let mas = [1, true, "asdsad", undefined, function mas() {console.log('fad')},[{name: "Karl", age: 5, happy: true}, { }] ];
+//
+//
+// console.log(mas[0]);
+// console.log(mas[mas.length - 1]);
+//
+//
+//  console.log(mas[4]());
+//  console.log(mas[5][0].name);
+//
+
+// const obj = {name: 'Mykola', age: 27};
+//
+// const {name, age} = obj;
+//
+// const name1 = name;
+// console.log(name1, age);
+
